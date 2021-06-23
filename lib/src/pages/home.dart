52c6138f5d1abac03306c5513b3a55f2606cc2b3
soohoin <soohoin12@naver.com>
2021-06-23 12:52:07 +0900

    Google 로그인 구현

--- lib/src/pages/home.dart
+++ lib/src/pages/home.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:test_sns_login/src/pages/login.dart';
 
 class Home extends StatelessWidget {
   const Home({Key key}) : super(key: key);
@@ -6,8 +8,21 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Firebase sns login"),
+      body: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (BuildContext context, AsyncSnapshot<User> snapshot) {
+          if (!snapshot.hasData) {
+            return Login();
+          } else {
+            return Center(
+              child: Column(
+                children: [
+                  Text("${snapshot.data.displayName}님 반갑습니다."),
+                ],
+              ),
+            );
+          }
+        },
       ),
     );
   }
