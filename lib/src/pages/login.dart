ed73330c5189c9becba898f6dbbb72c7fdf20ff1
soohoin <soohoin12@naver.com>
2021-06-28 11:55:49 +0900

    카카오 로그인 설정 테스트

--- lib/src/pages/login.dart
+++ lib/src/pages/login.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:uuid/uuid.dart';
+import 'package:flutter_web_auth/flutter_web_auth.dart';
 
 class Login extends StatelessWidget {
   const Login({Key key}) : super(key: key);
@@ -23,6 +25,23 @@ class Login extends StatelessWidget {
     return await FirebaseAuth.instance.signInWithCredential(credential);
   }
 
+  Future<UserCredential> signInWithKakao() async {
+    final clientState = Uuid().v4();
+    final url = Uri.https('kauth.kakao.com', '/oauth/authorize', {
+      'response_type': 'code',
+      'client_id': '5b81115ceee421c3220fe3e669fa3f34',
+      'redirect_uri': 'http://localhost:8080/kakao/sign_in',
+      'state': clientState,
+    });
+
+    final result = await FlutterWebAuth.authenticate(
+        url: url.toString(), callbackUrlScheme: "webauthcallback");
+
+    final body = Uri.parse(result).queryParameters;
+    print("print(body)");
+    print(body);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,7 +58,7 @@ class Login extends StatelessWidget {
             ),
             TextButton(
               child: Text("KaKao Login"),
-              onPressed: signInWithGoogle,
+              onPressed: signInWithKakao,
             ),
           ],
         ),
